c2fcbdd9417e89592021954c1f99c313a9cb6e6e
Jack <j.j.slettebak@gmail.com>
2020-10-21 21:14:49 -0700

    fixed primary key issue in follow dict

--- good_reads_follower.py
+++ good_reads_follower.py
@@ -35,7 +35,7 @@ class FollowManager:
                 await command_q.run_command(
                     channels[ self.follow_list[ entry ][ 'channel_id' ] ],
                     { 'fx': self.follow_updated_command },
-                    entry
+                    self.follow_list[ entry ][ 'user_id' ]
                 )
 
             self.__write_followers_list__( file_path )
@@ -49,21 +49,22 @@ class FollowManager:
             last_update = datetime.fromtimestamp( datetime.strptime( updates[ 0 ][ 'updated_at' ], DATE_TIME_FORMAT ).timestamp(), tz=timezone.utc ).timestamp()
         else:
             last_update = datetime.now( timezone.utc ).timestamp()
-        self.follow_list[ user_key ] = { 'channel_id': channel.id, 'last_update': last_update }
+        self.follow_list[ user_key + ":" + str( channel.id ) ] = { 'channel_id': channel.id, 'last_update': last_update, 'user_id': user_key }
         await channel.send( f'The {channel.name} channel on {channel.guild.name} is now following the GoodReads members {user_key}.' )
 
 
     async def follow_updated_command( self, channel, good_reads_client, user_key ):
+        key = user_key + ":" + str( channel.id )
         latest = 0
         ret = "";
         for update in resolve_user_activity( good_reads_client, user_key ):
             ts = datetime.fromtimestamp( datetime.strptime( update[ 'updated_at' ], DATE_TIME_FORMAT ).timestamp(), tz=timezone.utc ).timestamp()
-            if self.follow_list[ user_key ][ 'last_update' ] < ts:
+            if self.follow_list[ key ][ 'last_update' ] < ts:
                 await channel.send( f'GoodReads user {user_key} posted the following update:' )
                 await channel.send( embed = resolve_update_embed( user_key, update ) )
             if latest < ts:
                 latest = ts
-        self.follow_list[ user_key ][ 'last_update' ] = latest
+        self.follow_list[ key ][ 'last_update' ] = latest
         
 
 
